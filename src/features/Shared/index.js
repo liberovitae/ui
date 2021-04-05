a1b73473e7b13b2aa9d5a4b60d83afc5f6d69514
liberovitae <admin@liberovitae.com>
2021-04-06 00:30:15 +0100

    Add static date picker and index for shared components

---
+++ src/features/Shared/index.js
@@ -0,0 +1,22 @@
+import NotFound from './404';
+import Backdrop from './Backdrop';
+import BoxTemplate from './Box';
+import ContentDrawer from './ContentDrawer';
+import FAB from './FAB';
+import Hero from './Hero';
+import Loading from './Loading';
+import ScrollTop, { scrollTop } from './ScrollTop';
+import Snackbar from './Snackbar';
+
+export {
+  NotFound,
+  Backdrop,
+  BoxTemplate,
+  ContentDrawer,
+  FAB,
+  Hero,
+  Loading,
+  ScrollTop,
+  scrollTop,
+  Snackbar,
+};
