0c9f34332c331c51b45c6a282f1ec0cea5bae727
liberovitae <admin@liberovitae.com>
2021-04-06 18:00:17 +0100

    Fix no posts filename

--- src/features/Post/Posts/NoPosts.js
+++ src/features/Post/Posts/NoPosts.js
@@ -5,7 +5,7 @@ import { Typography, Box } from '@material-ui/core';
 const NoPosts = ({ type }) => (
   <Box p={4}>
     <Typography align="center" paragraph>
-      <img height={200} src="/img/noitems.png" />
+      <img height={200} src="/img/noposts.png" />
       <br />
       There are no {type}s yet -{' '}
       <Link to={`/${type}/create`}>be the first to post one!</Link>
